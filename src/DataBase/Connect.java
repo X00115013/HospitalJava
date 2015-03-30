3b0bc5d6f43f01a04326406000944a9747d002b6
X00115013 <X00115013@gmail.com>
2015-03-30 01:59:28 +0100

    All GUI done and connected..start on function and exception handing tomorrow. The rest of the files

---
+++ src/DataBase/Connect.java
@@ -0,0 +1,59 @@
+package DataBase;
+
+import oracle.jdbc.pool.OracleDataSource;
+
+import java.sql.*;
+
+/**
+ * Created by Roland on 14/03/2015.
+ */
+public class Connect {
+    private Connection conn;
+    private ResultSet rset;
+    private PreparedStatement pstmt;
+    private Statement stmt;
+
+    public Connect() {
+        conn = openDB();
+    }
+
+    // This method opens a connection to the Oracle database
+    public Connection openDB() {
+        try {
+            OracleDataSource ods = new OracleDataSource();
+
+//			// Tallaght
+//			ods.setURL("jdbc:oracle:thin:@//10.10.2.7:1521/global1");
+//            String uName = "X00115013";
+//            ods.setUser(uName);
+//            String uPass = "db02Dec77";
+//            ods.setPassword(uPass);
+
+//			Home Oracle XE
+            ods.setURL("jdbc:oracle:thin:hr/hr@localhost:1521/XE");
+            String uName = "SYSTEM";
+            ods.setUser(uName);
+            String uPass = "30905149";
+            ods.setPassword(uPass);
+
+            conn = ods.getConnection();
+            System.out.println("connected.");
+        } catch (Exception e) {
+            System.out.print("Unable to load driver " + e);
+            System.exit(1);
+        }
+        return conn;
+    }
+
+    // This method closes the connection to the Oracle database
+    public void closeDB() {
+        try {
+            conn.close();
+            System.out.print("Connection closed");
+        } catch (SQLException e) {
+            System.out.print("Could not close connection ");
+            e.printStackTrace();
+        }
+    }
+}
+
---
+++ src/GUI/AdminCheckOutGUI.java
@@ -0,0 +1,130 @@
+package GUI;
+
+import javax.swing.*;
+import javax.swing.border.BevelBorder;
+import javax.swing.border.Border;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+/**
+ * Created by Lionhart on 20/03/2015.
+ */
+public class AdminCheckOutGUI extends JFrame implements ActionListener
+{
+    JButton confirm,checkOut,cancel;
+    JLabel patientNum,label5;
+    JTextField patientText;
+    JTextArea additionalInformation;
+    JRadioButton checkOutRadio;
+
+    JFrame f;
+
+    public AdminCheckOutGUI()
+    {
+        f = new JFrame();
+        f.setTitle("Admin Check-Out");
+        f.setSize(700, 500);
+        f.setResizable(true);
+        f.setLocationRelativeTo(null);
+        f.setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
+
+        Border loweredBorder = BorderFactory.createBevelBorder(BevelBorder.LOWERED);
+
+        JPanel holder=new JPanel(new GridLayout(3,1));
+        JPanel topSection=new JPanel(new GridLayout(1,3));
+
+        Clock.DigitalClock clockD=new Clock.DigitalClock();
+        JPanel clock = new JPanel(new FlowLayout(FlowLayout.LEFT));
+        clock.add(clockD);
+
+        JPanel title=new JPanel(new FlowLayout(FlowLayout.CENTER));
+        label5 = new JLabel("Admin Check-Out");
+        title.add(label5);
+        label5.setFont(new Font("Arial", Font.BOLD, 24));
+
+        JPanel ID=new JPanel(new FlowLayout(FlowLayout.RIGHT));
+        //labels
+        patientNum = new JLabel("\tPatient Number");
+        ID.add(patientNum);
+        //text field
+        patientText = new JTextField(5);
+        patientText.setBorder(loweredBorder);
+        ID.add(patientText);
+
+
+        topSection.add(clock);
+        topSection.add(title);
+        topSection.add(ID);
+
+        holder.add(topSection);
+
+
+        JPanel textArea=new JPanel(new FlowLayout(FlowLayout.CENTER));
+
+        additionalInformation = new JTextArea(230,60);
+        additionalInformation.setBorder(loweredBorder);
+        textArea.add(additionalInformation);
+        holder.add(textArea);
+
+        //DOB labels
+        JPanel test =new JPanel(new FlowLayout(FlowLayout.LEFT));
+        JPanel dobs = new JPanel(new GridBagLayout());
+
+        // Confirm button
+        confirm = new JButton("Confirm");
+        confirm.addActionListener(this);
+        dobs.add(confirm, getConstraints(0, 3, 1, 1, GridBagConstraints.WEST));
+
+        // Cancel button
+        cancel = new JButton("Cancel");
+        cancel.addActionListener(this);
+        dobs.add(cancel, getConstraints(1, 3, 1, 1, GridBagConstraints.WEST));
+
+        // Cancel button
+        checkOut = new JButton("Generate Bill");
+        checkOut.addActionListener(this);
+        dobs.add(checkOut, getConstraints(2, 3, 1, 1, GridBagConstraints.WEST));
+
+
+        checkOutRadio = new JRadioButton("Check-Out");
+        checkOutRadio.addActionListener(this);
+        dobs.add(checkOutRadio);
+        test.add(dobs);
+        holder.add(test);
+
+        f.add(holder);
+        f.setVisible(true);
+    }
+
+
+
+
+    private GridBagConstraints getConstraints(int gridx, int gridy, int gridwidth, int gridheight, int anchor) {
+        GridBagConstraints c = new GridBagConstraints();
+        c.insets = new Insets(10, 5, 10, 10);
+        c.ipadx = 0;
+        c.ipady = 0;
+        c.gridx = gridx;
+        c.gridy = gridy;
+        c.gridwidth = gridwidth;
+        c.gridheight = gridheight;
+        c.anchor = anchor;
+        return c;
+    }
+
+
+    public void actionPerformed(ActionEvent e)
+    {
+        if (e.getSource().equals(cancel))
+        {
+            System.exit(0);
+        }
+        else if (e.getSource().equals(confirm))
+        {
+
+        }else if (e.getSource().equals(checkOut))
+        {PaymentGUI paymentGUI=new PaymentGUI();
+        }
+    }
+}
